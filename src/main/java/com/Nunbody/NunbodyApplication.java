756c4b1b623419d0758d3572b058879fcdc23d08
jinseok <qkrwlstjr0131@naver.com>
2023-08-24 15:24:18 +0900

    setting

---
+++ src/main/java/com/Nunbody/NunbodyApplication.java
@@ -0,0 +1,13 @@
+package com.Nunbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunbodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NunbodyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Nunbody/NunbodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nunbody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NunbodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
