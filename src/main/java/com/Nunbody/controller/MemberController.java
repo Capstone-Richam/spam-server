1b46ce34d5d8dede0a71f2e47fc6c9948fdd9387
jinseok <qkrwlstjr0131@naver.com>
2023-10-13 21:46:25 +0900

    [chore]:Main #2 rds연동

---
+++ src/main/java/com/Nunbody/controller/MemberController.java
@@ -0,0 +1,12 @@
+package com.Nunbody.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+    @PostMapping("/user")
+    public ResponseEntity<?> create(@RequestBody )
+}
