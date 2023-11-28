4a657e43c080f5b6ed102a975c0f30db38281d33
jinseok <qkrwlstjr0131@naver.com>
2023-11-28 13:48:24 +0900

    d

--- src/main/java/com/Nunbody/domain/member/dto/MemberRegisterResponseDto.java
+++ src/main/java/com/Nunbody/domain/member/dto/MemberRegisterResponseDto.java
@@ -8,15 +8,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberRegisterResponseDto {
-    private Long memberId;
+    private Long id;
     private String account;
     private String password;
     private String name;
     private String naverId;
     private String naverPassword;
-    private boolean hasNaver;
     private String gmailId;
     private String gmailPassword;
-    private boolean hasGmail;
 
 }
