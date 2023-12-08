c6f28bee4b07b2a628501e118dc2b7dc51113684
Bae Hyeonseo <haulqogustj@naver.com>
2023-12-09 00:19:25 +0900

    [feat]: Main 메일 필터링 키워드 추가

--- src/main/java/com/Nunbody/domain/Mail/controller/MailFilterController.java
+++ src/main/java/com/Nunbody/domain/Mail/controller/MailFilterController.java
@@ -1,10 +1,14 @@
 package com.Nunbody.domain.Mail.controller;
 
+import com.Nunbody.domain.Mail.dto.response.FilterMailListResponseDto;
 import com.Nunbody.domain.Mail.dto.response.MailListResponseDto;
 import com.Nunbody.domain.Mail.dto.resquest.FilterKeywordRequest;
 import com.Nunbody.domain.Mail.service.FilterService;
 import com.Nunbody.global.common.SuccessResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,14 +21,14 @@ import java.util.List;
 public class MailFilterController {
     private final FilterService filterService;
     @PostMapping
-    public ResponseEntity<SuccessResponse<?>> filtertest(@RequestBody FilterKeywordRequest filterKeywordRequest){
-        List<MailListResponseDto> mailListResponseDtoList = filterService.filterContent(filterKeywordRequest);
+    public ResponseEntity<SuccessResponse<?>> filtertest(@RequestBody FilterKeywordRequest filterKeywordRequest, @PageableDefault Pageable pageable){
+        Page<FilterMailListResponseDto> mailListResponseDtoList = filterService.filterContent(filterKeywordRequest,pageable);
         return SuccessResponse.ok(mailListResponseDtoList);
     }
 
-    @PostMapping("/nlp")
-    public ResponseEntity<SuccessResponse<?>> nlpFilter(@RequestBody FilterKeywordRequest filterKeywordRequest) throws IOException {
-        List<String> list = filterService.categoryFilter(filterKeywordRequest);
-        return SuccessResponse.ok(list);
-    }
+//    @PostMapping("/nlp")
+//    public ResponseEntity<SuccessResponse<?>> nlpFilter(@RequestBody FilterKeywordRequest filterKeywordRequest) throws IOException {
+//        List<String> list = filterService.categoryFilter(filterKeywordRequest);
+//        return SuccessResponse.ok(list);
+//    }
 }
