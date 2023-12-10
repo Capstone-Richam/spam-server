776d400cfbaa7cf5c99fce364ce42556910de7be
Bae Hyeonseo <haulqogustj@naver.com>
2023-12-10 21:43:14 +0900

    [fix]: ?

--- src/main/java/com/Nunbody/domain/Mail/controller/MailFilterController.java
+++ src/main/java/com/Nunbody/domain/Mail/controller/MailFilterController.java
@@ -12,6 +12,9 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/filter")
@@ -23,9 +26,9 @@ public class MailFilterController {
         return SuccessResponse.ok(mailListResponseDtoList);
     }
 
-//    @PostMapping("/nlp")
-//    public ResponseEntity<SuccessResponse<?>> nlpFilter(@RequestBody FilterKeywordRequest filterKeywordRequest) throws IOException {
-//        List<String> list = filterService.categoryFilter(filterKeywordRequest);
-//        return SuccessResponse.ok(list);
-//    }
+    @PostMapping("/nlp")
+    public ResponseEntity<SuccessResponse<?>> nlpFilter(@RequestParam Long memberId, @RequestBody FilterKeywordRequest filterKeywordRequest) throws IOException {
+        List<String> list = filterService.categoryFilter(memberId, filterKeywordRequest);
+        return SuccessResponse.ok(list);
+    }
 }
