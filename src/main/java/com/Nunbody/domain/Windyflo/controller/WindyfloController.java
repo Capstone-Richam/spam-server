74e2986e0a500ef5431114603b9ef50188a696af
qogustj <haulqogustj@naver.com>
2024-07-02 11:00:16 +0900

    [feat] 텍스트 추출

--- src/main/java/com/Nunbody/domain/Windyflo/controller/WindyfloController.java
+++ src/main/java/com/Nunbody/domain/Windyflo/controller/WindyfloController.java
@@ -1,6 +1,7 @@
 package com.Nunbody.domain.Windyflo.controller;
 
 import com.Nunbody.domain.Windyflo.dto.req.WindyfloReq;
+import com.Nunbody.domain.Windyflo.dto.res.EmailResDto;
 import com.Nunbody.domain.Windyflo.service.WindyfloService;
 import com.Nunbody.external.ConversationQARes;
 import com.Nunbody.global.common.SuccessResponse;
@@ -8,14 +9,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/windyflo")
 public class WindyfloController {
     private final WindyfloService windyfloService;
     @PostMapping
-    public ResponseEntity<SuccessResponse<?>> createMail(@RequestBody WindyfloReq windyfloReq){
-        ConversationQARes conversationQARes = windyfloService.createMail(windyfloReq);
-        return SuccessResponse.ok(conversationQARes);
+    public ResponseEntity<SuccessResponse<?>> createMail(@RequestBody WindyfloReq windyfloReq) throws IOException {
+        EmailResDto emailResDto = windyfloService.createMail(windyfloReq);
+        return SuccessResponse.ok(emailResDto);
     }
 }
